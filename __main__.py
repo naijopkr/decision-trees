8f62ed9e4377da1222d3721f6ad3b17f0100f775
Ariel Barcellos <barcellos.ariel@gmail.com>
2020-06-22 15:37:54 -0500

    implement decision tree and tree visualization

--- __main__.py
+++ __main__.py
@@ -1,3 +1,50 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+df = pd.read_csv('./data/kyphosis.csv')
+df.head()
+
+sns.pairplot(df, hue='Kyphosis')
+
+from sklearn.model_selection import train_test_split
+
+X = df.drop('Kyphosis', axis=1)
+y = df['Kyphosis']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+# Decision trees
+from sklearn.tree import DecisionTreeClassifier
+
+dtree = DecisionTreeClassifier()
+dtree.fit(X_train, y_train)
+
+y_pred = dtree.predict(X_test)
+
+# Evaluation
+from sklearn.metrics import confusion_matrix, classification_report
+
+print(confusion_matrix(y_test, y_pred))
+print()
+print(classification_report(y_test, y_pred))
+
+# Tree visualization
+from IPython.display import Image
+from io import StringIO
+from sklearn.tree import export_graphviz
+import pydot
+
+features = list(df.columns[1:])
+features
+
+dot_data = StringIO()
+export_graphviz(
+    dtree,
+    out_file=dot_data,
+    feature_names=features,
+    filled=True,
+    rounded=True
+)
+graph = pydot.graph_from_dot_data(dot_data.getvalue())
+Image(graph[0].create_png())
